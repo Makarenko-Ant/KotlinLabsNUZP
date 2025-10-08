4d5221248a8747d060e0d1156559626b2bc9cbe9
Makarenko-Ant <makarenkoanton1507@gmail.com>
2025-10-08 16:12:34 +0300

    лабораторна робота 2

--- helloworld/build.gradle.kts
+++ helloworld/build.gradle.kts
@@ -1,5 +1,6 @@
 plugins {
-    kotlin("jvm") version "1.8.0"
+    kotlin("jvm") version Versions.kotlin
+    id("org.jetbrains.compose") version Versions.compose
     application
 }
 
@@ -8,15 +9,24 @@ version = "1.0-SNAPSHOT"
 
 repositories {
     mavenCentral()
+    maven("https://maven.pkg.jetbrains.space/public/p/compose/dev")
+    maven("file://${rootDir}/.m2repo/")
 }
 
 dependencies {
+    implementation(Versions.library)
+    implementation(compose.desktop.currentOs)
     testImplementation(kotlin("test"))
 }
 
 tasks.test {
     useJUnitPlatform()
 }
+
 kotlin {
-    jvmToolchain(17)
+    jvmToolchain(Versions.jvmLevel)
+}
+
+application {
+    mainClass.set("MainKt")
 }
--- helloworld/src/main/kotlin/Main.kt
+++ helloworld/src/main/kotlin/Main.kt
@@ -1,21 +1,59 @@
+import com.diacht.ktest.compose.startTestUi
+import org.example.helloworld.BuildConfig
+import kotlin.math.*
+
 fun seed(): String = "Makarenko-Ant"
 
-fun labNumber(): Int = 1
+fun labNumber(): Int = BuildConfig.LAB_NUMBER
+
+fun iCalculate(
+    x0: Int = 46,
+    x1: Int = -60,
+    x2: Int = 55,
+    x3: Int = -112
+) {
+    val x = x0*x0 + x1*x1 + x2*x2 + x3*x3
+    val res: Double = sin(x.toDouble())
+    println(res)
+}
+
+fun dCalculate(
+    x0: Double = -57.27,
+    x1: Double = 82.49,
+    x2: Double = -39.99,
+    x3: Double = -83.52,
+    x4: Double = 6.05
+) {
+    val x = minOf(abs(x0), abs(x1), abs(x2), abs(x3), abs(x4))
+    val res = cbrt(x)
+    println(res)
+}
+
+fun strCalculatet(
+    x0: String,
+    x1: String
+): Int {
+    require(x0.length == x1.length && x0.length % 2 == 0){
+        "Рядки повинні мати однакову парну довжину"
+    }
+    var diffCount = 0;
+    for (i in x0.indices step 2) {
+        val c0 = x0[i]
+        val c1 = x1[i]
+        val areEqual = when {
+            (c0 == 'T' && c1 == 'J') || (c0 == 'J' && c1 == 'T') -> true
+            else -> c0 == c1
+        }
+        if (!areEqual) {diffCount++}
+    }
+    return diffCount
+}
 
 fun main(args: Array<String>) {
     println("Лабораторна робота №${labNumber()} користувача ${seed()}")
+    iCalculate()
+    dCalculate()
+    print(strCalculatet("ATCCTJ","AJGJJT"))
+    //startTestUi(seed(), labNumber())
 
-    var kitty = "Васько"
-    kitty += " \uD83D\uDC31"
-    val age = 7
-    println("Кошеня №1 - $kitty віком $age років")
-
-    val catName: String = "Мурзик \uD83D\uDC08"
-    val weight: Float = 3.5f
-    println("Кошеня №2 - $catName з вагою $weight кг")
-
-    val catName2: String = "Рудий \uD83D\uDC06"
-    val weight2: Float = 8.2f
-    val age2 = 6
-    println("Кошеня №3 - $catName2 віком $age2 та вагою $weight2 кг")
 }
